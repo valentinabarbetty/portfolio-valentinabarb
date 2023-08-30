757a722b27ee2d2a5fdce493f8f3e7a7c394ba32
valentinabarbetty <113564513+valentinabarbetty@users.noreply.github.com>
2023-08-30 11:00:10 -0500

    Box implementation

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { createRoot} from "react-dom/client";
+import Experience from "./Experience";
+import "./styles.css"
+import { Canvas } from "@react-three/fiber";
+
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <>
+    <Canvas camera={
+            {
+                position: [2, 0, 5]
+            }
+        }>
+        
+        <Experience />
+    </Canvas>
+    </>
+    
+);
