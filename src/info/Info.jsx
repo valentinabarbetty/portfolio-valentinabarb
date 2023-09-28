58b3a439636e527dd8580e9527bd4f6184a4575b
valentinabarbetty <113564513+valentinabarbetty@users.noreply.github.com>
2023-09-27 20:47:47 -0500

    Added onclick and typing effect

--- src/info/Info.jsx
+++ src/info/Info.jsx
@@ -1,13 +1,28 @@
 import "./info.css";
-
-const Info = ({ name, biography }) => {
+import Typed from "react-typed";
+const Info = ({ name, biography, show }) => {
   return (
-    <div className="container-info">
-      <div>
-        <h1 className="name">Hi there! I'm {name}</h1>
-        <p className="bio">{biography}</p>
+    // <div className="container-info">
+    //   <div>
+    //     <h1 className="name">Hi there! I'm {name}</h1>
+    //     <p className="bio">{biography}</p>
+    //   </div>
+    // </div>
+
+    !show ? (
+      <div className="container-info">
+        <div className="animated-typing bio">
+          <h1 className="name">Hi there! I'm {name}</h1>
+
+          <Typed
+            strings={[biography]}
+            typeSpeed={80}
+            // backSpeed={100}
+            // loop
+          />
+        </div>
       </div>
-    </div>
+    ) : null
   );
 };
 
