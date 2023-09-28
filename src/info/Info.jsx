6c73eadc745576cc37887903479bc4d508ac37aa
valentinabarbetty <113564513+valentinabarbetty@users.noreply.github.com>
2023-09-27 22:11:38 -0500

    typewriter

--- src/info/Info.jsx
+++ src/info/Info.jsx
@@ -1,25 +1,24 @@
 import "./info.css";
-import Typed from "react-typed";
+import Typewriter from "typewriter-effect";
 const Info = ({ name, biography, show }) => {
+  
   return (
-    // <div className="container-info">
-    //   <div>
-    //     <h1 className="name">Hi there! I'm {name}</h1>
-    //     <p className="bio">{biography}</p>
-    //   </div>
-    // </div>
+   
 
     !show ? (
       <div className="container-info">
         <div className="animated-typing bio">
           <h1 className="name">Hi there! I'm {name}</h1>
-
-          <Typed
-            strings={[biography]}
-            typeSpeed={80}
-            // backSpeed={100}
-            // loop
-          />
+          <Typewriter
+ 
+                onInit={(typewriter) => {
+                    typewriter
+                        .typeString(biography)
+                        .changeDelay(0.5)
+                        .start();
+                }}
+            />
+         
         </div>
       </div>
     ) : null
